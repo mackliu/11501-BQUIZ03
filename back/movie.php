6e585965fe7663ee77dede3d7054bff5f328c22e
mackliu <macklun@gmail.com>
2026-07-06 14:06:14 +0800

    改造api/sw.php 成為可以共用的交換排序api

--- back/movie.php
+++ back/movie.php
@@ -14,7 +14,7 @@
 <div class="movies">
     <?php 
     $movies=$Movie->all(" order by rank");
-    foreach($movies as $movie):
+    foreach($movies as $idx => $movie):
     ?>
     <div class="movie">
         <div>
@@ -31,8 +31,13 @@
             </div>
             <div style="text-align:right;padding:5px;">
                 <button data-id="<?= $movie['id'] ?>">顯示</button>
-                <button class="switch-rank" data-sw="">往上</button>
-                <button class="switch-rank" data-sw="">往下</button>
+                <?php 
+                $prev=($idx==0)?$movie['id']:$movies[$idx-1]['id'];
+                $next=($idx==count($movies)-1)?$movie['id']:$movies[$idx+1]['id'];
+            ?>
+            <input class="switch-rank" type="button" value="往上" data-ids="<?= $movie['id']."-".$prev ?>">
+            <input class="switch-rank" type="button" value="往下" data-ids="<?= $movie['id']."-".$next ?>">
+                
                 <button onclick="location.href='?do=edit_movie&id=<?= $movie['id'] ?>'">編輯電影</button>
                 <button data-id="<?= $movie['id'] ?>">刪除電影</button>
             </div>
@@ -45,3 +50,12 @@
     endforeach;
     ?>
 </div>
+
+<script>
+$(".switch-rank").on("click",function(){
+    let ids=$(this).data('ids').split('-')
+    $.post("./api/sw.php",{ids,'table':"Movie"},()=>{
+        location.reload()
+    })
+})
+</script>
