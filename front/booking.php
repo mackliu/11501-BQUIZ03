542b071af805a6dbed87480712ccd418baf8456a
mackliu <macklun@gmail.com>
2026-07-13 09:32:13 +0800

    建立訂票選單畫面及外觀

---
+++ front/booking.php
@@ -0,0 +1,47 @@
+<style>
+.booking-form{
+    width:450px;
+    margin:20px auto;
+    padding:20px;
+    background:#eee;
+    border:1px solid #ccc;
+    /* border-collapse: collapse; */
+}
+.booking-form tr:nth-child(even){
+    background:#ccc;
+}
+.booking-form td{
+    padding:5px;
+    border:1px solid #ccc;
+}    
+.booking-form td select{
+    width:98%;
+}
+</style>
+
+
+<h3 class="ct">線上訂票</h3>
+<table class="booking-form">
+    <tr>
+        <td style="width:60px;">電影:</td>
+        <td>
+            <select name="movie" id="movie"></select>
+        </td>
+    </tr>
+    <tr>
+        <td>日期:</td>
+        <td>
+            <select name="date" id="date"></select>
+        </td>
+    </tr>
+    <tr>
+        <td>場次:</td>
+        <td>
+            <select name="session" id="session"></select>
+        </td>
+    </tr>
+</table>
+<div class="ct">
+    <button class='btn-submit'>確定</button>
+    <button class='btn-reset'>重置</button>
+</div>
